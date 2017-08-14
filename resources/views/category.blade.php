2a218bdb1209a1dc8ebcb3eb4ab0d48657b0601a
Turisap <kirill-shakirov@mail.ru>
2017-08-14 08:15:56 +0700

    Category Page

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -22,6 +22,7 @@
                                         <h3>{{$main_quiz->title}}</h3>
                                         <h4>{{$main_quiz->description}}</h4>
                                         <p>By {{$main_quiz->author->first_name}} {{$main_quiz->author->last_name}}</p>
+                                        <a href="/quizzes/play/{{$main_quiz->id}}" class="quiz-link btn btn-default">PLAY THIS QUIZ</a>
                                     </div>
                                     <div class="col-md-4" id="image">
                                         @if($main_quiz->url)
@@ -29,11 +30,6 @@
                                         @else
                                             <img class="img-responsive img-circle" src="/images/static/images.png">
                                         @endif
-                                    </div>
-                                </div>
-                                <div class="row button-play">
-                                    <div class="col-md-12">
-                                        <a href="/quizzes/play/{{$main_quiz->id}}" class="quiz-link btn btn-default">PLAY THIS QUIZ</a>
                                     </div>
                                 </div>
                             </div>
