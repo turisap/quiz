4e91099dbe3604255968f60dc0ec2c3b2b965381
Turisap <kirill-shakirov@mail.ru>
2017-08-14 11:40:07 +0700

    Profile page

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,26 +1,21 @@
 @extends('baseviews.base')
 
 @section('content')
-         <section id="profileHeading">
-             <div class="container-fluid">
-                 <div class="row">
-                     <div class="col-md-12">
-                         <h2 class="page-header">Your info</h2>
-                     </div>
-                 </div>
-             </div>
-         </section>
-
-        <section class="profileInfo">
+        <section id="profileInfo">
             <div class="container-fluid">
             <div class="row">
-                <div class="col-md-8 col-md-offset-2">
-                    <div class="row">
-                        <div class="col-md-4">
-                            <img class="img-responsive pull-right" @if(!$url) src="/images/static/images.png" @else src="{{$url}}" @endif >
+                <div class="col-md-4">
+                    <img src="/images/static/settings.png" id="img2">
+                </div>
+                <div class="col-md-4 panel panel-default">
+                    <div class="row panel-heading">
+                        <div class="col-md-12" id="profilePicture">
+                            <img class="img-rounded" @if(!$url) src="/images/static/images.png" @else src="{{$url}}" @endif >
                         </div>
-                        <div class="col-md-8">
-                            <dl class="list-group left-bordered">
+                    </div>
+                    <div class="row panel-body">
+                        <div class="col-md-6">
+                            <dl class="list-group">
                                 <dt>Student:</dt>
                                 <dd>{{$profile->first_name}} {{$profile->last_name}}</dd>
                                 <dt>School:</dt>
@@ -29,6 +24,10 @@
                                 <dd>{{$profile->grade}}</dd>
                                 <dt>Age:</dt>
                                 <dd>{{$profile->age}}</dd>
+                            </dl>
+                        </div>
+                        <div class="col-md-6">
+                            <dl class="list-group">
                                 <dt>Gender:</dt>
                                 <dd>{{$profile->gender}}</dd>
                                 <dt>Favorite Subject:</dt>
@@ -36,9 +35,12 @@
                                 <dt>Shortly about:</dt>
                                 <dd>{{$profile->notes}}</dd>
                             </dl>
-                            <a class="btn btn-default" href="/profile/{{$profile->id}}/edit">Update</a>
                         </div>
+                        <a class="btn btn-default pull-right" href="/profile/{{$profile->id}}/edit">Update</a>
                     </div>
+                </div>
+                <div class="col-md-4">
+                    <img src="/images/static/settings1.png" id="img4">
                 </div>
             </div>
             </div>
