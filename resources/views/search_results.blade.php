1e38c2b744baa11edc50d5c298b907f22f047d38
Turisap <kirill-shakirov@mail.ru>
2017-08-17 11:07:07 +0700

    Ready App

--- resources/views/search_results.blade.php
+++ resources/views/search_results.blade.php
@@ -1,14 +1,14 @@
 @extends('baseviews/base')
 
 @section('content')
-    <section id="mostPopular">
+    <section id="searchResults">
         <div class="row">
             <div class="col-md-12 text-center">
                 <h2 class="page-header">Search Results</h2>
             </div>
         </div>
         <div class="container-fluid">
-            <div class="wow fadeIn">
+            <div class="wow fadeIn text-center">
                 @foreach($chunks as $chunk)
                     <div class="row">
                         @foreach($chunk as $quiz)
@@ -16,6 +16,9 @@
                         @endforeach
                     </div>
                 @endforeach
+                @if(!$chunks->first())
+                    <h3>There is nothing found according to your search</h3>
+                @endif
             </div>
         </div>
     </section>
